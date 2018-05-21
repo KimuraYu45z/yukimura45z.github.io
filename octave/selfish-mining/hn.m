f7812ae5ad1995e2c83b3c7cec3f13d492a138be
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-05-21 09:46:44 +0900

    update

--- octave/selfish-mining/hn.m
+++ octave/selfish-mining/hn.m
@@ -3,14 +3,15 @@ H = 1750;
 R = 25;
 x = 100;
 
-[h, n] = meshgrid(1: 700, 1 : 100);
+[h, n] = meshgrid(1:1000, 1 : 100);
 
 p = mining_probability(h, H);
 sp = catching_up_probability(n, 0, p);
 
 selfish_mining_condition = sp .* (R + x) - p .* R;
 
-figure;
-surf(selfish_mining_condition);
-
+surf(selfish_mining_condition, 'EdgeColor', 'none');
+xlabel('hash rate');
+ylabel('confirmation');
+zlabel('incentive');
 colorbar;
--- octave/selfish-mining/nx.m
+++ octave/selfish-mining/nx.m
@@ -3,14 +3,15 @@ H = 1750;
 
 R = 25;
 
-[x, n] = meshgrid(1: 1000, 1 : 100);
+[x, n] = meshgrid(1:1000, 1 : 100);
 
 p = mining_probability(h, H);
 sp = catching_up_probability(n, 0, p);
 
 selfish_mining_condition = sp .* (R + x) - p .* R;
 
-figure;
-surf(selfish_mining_condition);
-
+surf(selfish_mining_condition, 'EdgeColor', 'none');
+xlabel('transaction amount');
+ylabel('confirmation');
+zlabel('incentive');
 colorbar;
